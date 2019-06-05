0ca4aaaacad68497d46f087dd0b15d0a8833d6be
Mark <mark.steininger@gmail.com>
2019-06-05 15:04:52 -0400

    rewrote the draw function to place svg rectangles inside the graph instead of colored divs. Added axises, and changed from horizontal histogram to verticle

--- JS/draw.js
+++ JS/draw.js
@@ -3,47 +3,57 @@ function draw(data){
       D3.js setup code
      */
     "use strict";
-    const margin = 75,
-        width = 1400 - margin,
-        height = 600 - margin;
+    //set some constants
+    let svg = d3.select("svg"),
+        margin = 200,
+        width = svg.attr("width") - margin,
+        height = svg.attr("height") - margin;
 
-    let x = d3.scaleLinear()
-        .range([0, width]);
+    //set the scale and range(pixel space available)
+    //y range is always backwards in d2 because dom renders top to bottom
+    let yScale = d3.scaleLinear().range([height, 0]),
+        //band scale into catagories
+        xScale = d3.scaleBand().range([0,width]).paddingInner(0.4);
 
-    x.domain([0,17.12]);
+    //add group element to place things inside of
+    let g = svg.append("g")
+        .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
-    let xAxis = d3.axisBottom(x);
-
-
-    let svg = d3.select("body")
-        .append("svg")
-        .style("width", width + margin)
-        .style("height", height + margin)
-        .append('g')
-            .attr('class','chart');
-
-
-
+    //get the data for 1970
     let restructured = restructure_data(data, 1970);
 
+    //set the domain based on the restructured data (extent of the data values to map to the scale)
+    xScale.domain(restructured.map(function(d) { return d.Country; }));
+    yScale.domain([0, 18]);
 
-    d3.select("svg")
-        .selectAll("div")
+
+    //add axis to bottom
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+    //add axis to the side
+    g.append("g")
+        .call(d3.axisLeft(yScale).tickFormat(function(d){
+            return d + "%";
+        }).ticks(10))
+        .append("text")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("value");
+
+    //add the bars, svg rect
+    g.selectAll(".bar")
         .data(restructured)
         .enter()
-        .append("div")
-        .style('width', function(d){
-            if(d['Spending'] === "null"){
-                return '5px';
-            }
-            return d['Spending'] * 80 + 'px';
-        })
-        .style("height", "50px")
-        .style("background-color", "lightBlue")
-        .style("border", "1px solid black")
-        .text(function(d){
-            return d["Country"];
-        });
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return xScale(d.Country); })
+        .attr("y", function(d) { return yScale(d.Spending); })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function(d) { return height - yScale(d.Spending); })
+        .attr("fill", "black");
 
-    begin_counting_years(data);
+    //begin_counting_years(data);
 }
